21c4bf2374f2259e9733cffdcc6d4d52ccba706a
Travis <travismay@gmail.com>
2014-09-08 11:44:31 -0500

    Modify msio script

--- msio_format.R
+++ msio_format.R
@@ -2,7 +2,6 @@ for (i in 1:8) {
 table <- paste("Table", as.character(i), sep=" ")
 worksheets <- list()
 assign(paste("Table",i,sep=""), worksheets[[i]])
-get(worksheets[[i]])
-# <- read.xls("msio_full.xls", table)
+get(worksheets[[i]]) <- read.xls("msio_full.xls", table)
 }
 
